df9dfdffd5a11a0805fc50c1e07f36da84600a2e
asia-jankowska00 <asia.jankowska00@gmail.com>
2020-12-23 17:34:54 +0100

    chore: add strapi

---
+++ packages/server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bdac3691d293b0beaa400c4cd28e0078'),
+    },
+  },
+});
---
+++ packages/server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd79a50e4-102e-486f-95da-8ea6f8164848'
+};
