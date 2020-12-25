9a40aa68475f02bafff12159b15e6dd52489022f
asia-jankowska00 <asia.jankowska00@gmail.com>
2020-12-25 10:18:03 +0100

    chore: add fontawesome

--- packages/business-client/src/main.js
+++ packages/business-client/src/main.js
@@ -4,13 +4,19 @@ import store from './store'
 import router from './router'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-Vue.use(Buefy)
+library.add(fas)
+
+Vue.use(Buefy, { defaultIconComponent: 'vue-fontawesome', defaultIconPack: 'fas' })
+Vue.component('vue-fontawesome', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
   router,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
