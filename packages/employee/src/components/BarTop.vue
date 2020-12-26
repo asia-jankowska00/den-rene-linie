8637c11f415f5ba0662cfb345f306c4198341065
asia-jankowska00 <asia.jankowska00@gmail.com>
2020-12-26 14:48:32 +0100

    feat(eme): add base layout and base router

---
+++ packages/employee/src/components/BarTop.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tabs is-toggle is-fullwidth">
+    <ul>
+      <router-link tag="li" to="/dashboard/profile" active-class="is-active" exact>
+        <a class="is-flex-direction-column">
+          <b-icon pack="fas" icon="user-cog" />
+          <span>Profile</span>
+        </a>
+      </router-link>
+      <li>
+        <a class="logo">
+          <span>logo</span>
+        </a>
+      </li>
+      <router-link tag="li" to="/dashboard/chat" active-class="is-active" exact>
+        <a class="is-flex-direction-column">
+          <b-icon pack="fas" icon="comments" />
+          <span>Chat</span>
+        </a>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default { name: 'BarTop' }
+</script>
+
+<style>
+
+</style>
---
+++ packages/employee/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-field label="Username">
+      <b-input value="johnsilver" maxlength="30"></b-input>
+    </b-field>
+
+    <b-field label="Password">
+      <b-input type="password" password-reveal></b-input>
+    </b-field>
+
+    <b-button tag="router-link" to="/dashboard">Log in</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  components: {}
+}
+</script>
