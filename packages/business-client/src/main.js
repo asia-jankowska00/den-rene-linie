486de95db4935b299c323bc9aabe747b4c25b7ef
asia-jankowska00 <asia.jankowska00@gmail.com>
2021-01-06 20:39:23 +0100

    feat(bcl): add formatting helpers

--- packages/business-client/src/main.js
+++ packages/business-client/src/main.js
@@ -7,6 +7,7 @@ import 'buefy/dist/buefy.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import dayjs from 'dayjs'
 
 library.add(fas)
 
@@ -14,6 +15,30 @@ Vue.use(Buefy, { defaultIconComponent: 'vue-fontawesome', defaultIconPack: 'fas'
 Vue.component('vue-fontawesome', FontAwesomeIcon)
 
 Vue.config.productionTip = false
+
+const baseUrl = 'http://localhost:1337'
+
+Vue.mixin({
+  data() {
+    return {
+      placeholderAvatar: `${baseUrl}/uploads/Portrait_Placeholder_b9c75025d0.png`
+    }
+  },
+  methods: {
+    formatName(user) {
+      return `${user.firstName} ${user.lastName}`
+    },
+    formatDate(date) {
+      return dayjs(date).format('DD/MM/YYYY')
+    },
+    formatDateTime(date) {
+      return dayjs(date).format('DD/MM/YYYY, hh:mm')
+    },
+    formatDuration(date) {
+      return dayjs(date).format('hh:mm')
+    }
+  }
+})
 
 new Vue({
   store,
