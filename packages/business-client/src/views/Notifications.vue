6fc38841339cbd61ce8266504dff37563fce484b
asia-jankowska00 <asia.jankowska00@gmail.com>
2021-01-08 21:59:07 +0100

    feat(bcl): add Notifications layout

--- packages/business-client/src/views/Notifications.vue
+++ packages/business-client/src/views/Notifications.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>Notifications</div>
+  <div>
+    <div class="content">
+      <h2>Notifications</h2>
+      <router-link tag="article" to="/dashboard/bookings" class="card is-shadowless">
+        <div class="content">
+          <strong> status booking</strong>
+          <p>
+            <strong>client</strong>
+            ’s booking from
+            <strong>startDate</strong>
+            to
+            <strong>endDate</strong>
+            has been
+            <strong>action</strong>
+            .
+          </p>
+          <small></small>
+        </div>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
