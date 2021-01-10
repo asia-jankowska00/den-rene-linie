fba0aa683baf3c67abaecc7a299cb7dba374d8bf
asia-jankowska00 <asia.jankowska00@gmail.com>
2021-01-10 21:36:32 +0100

    refactor(eme): cleanup Guides

--- packages/employee/src/components/BarBottom.vue
+++ packages/employee/src/components/BarBottom.vue
@@ -8,7 +8,7 @@
             <span>Bookings</span>
           </a>
         </router-link>
-        <router-link tag="li" to="/dashboard/services" active-class="is-active" exact>
+        <router-link tag="li" to="/dashboard/guides" active-class="is-active" exact>
           <a class="is-flex-direction-column">
             <b-icon pack="fas" icon="address-book" />
             <span>Guides</span>
@@ -31,6 +31,7 @@ export default { name: 'BarBottom' }
 
 <style lang="scss">
 .bar-bottom {
+  position: fixed;
   bottom: 0;
   left: 0;
   width: 100%;
@@ -38,5 +39,9 @@ export default { name: 'BarBottom' }
 
 .b-tabs .tab-content {
   padding: 0;
+}
+
+.tabs {
+  background: #ffffff;
 }
 </style>
---
+++ packages/employee/src/views/Guides.vue
@@ -0,0 +1,56 @@
+<template>
+  <section>
+    <div class="content">
+      <h2>Guides</h2>
+      <div class="columns is-mobile is-multiline is-3 is-variable">
+     <article
+          v-for="guide in guides"
+          :key="guide._id"
+          tag="article"
+          to=""
+          class="column is-half"
+        >
+          <div class="card">
+            <div class="is-16by9 is-flex is-justify-content-center">
+              <img class="image is-64x64 is-centered" :src="guide.image ? guide.image[0].url : placeholderAvatar" />
+            </div>
+            <div class="card-content">
+              <p class="title is-5 has-text-centered">{{ guide.name }}</p>
+            </div>
+            <footer class="card-footer">
+              <b-button
+                href="#"
+                class="card-footer-item"
+              >
+                View
+              </b-button>
+            </footer>
+          </div>
+        </article>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'Services',
+  computed: {
+    ...mapGetters('guides', ['guides'])
+  },
+  mounted() {
+    this.getGuides()
+  },
+  methods: {
+    ...mapActions('guides', ['getGuides'])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.columns {
+  margin-top: 0;
+}
+</style>
